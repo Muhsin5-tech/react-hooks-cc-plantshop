febcea2d050a7156d2ba61e86b96c2fb393962c4
Muhsin Ali <muhsin.ali@student.moringaschool.com>
2024-11-10 16:02:30 +0300

    Adds src/component/App.js

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [SearchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then((response) => response.json())
+    .then((data) => setPlants(data))
+  }, []);
+
+
+function handleAddPlant(newPlant) {
+  setPlants([...plants, newPlant])
+}
+
+function handleToggleSoldOut(id, soldOut) {
+  fetch(`http://localhost:6001/plants/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ soldOut }),
+  })
+  .then((response) => response.json())
+  .then((updatedPlant) => {
+    const updatedPlants = plants.map((plant) =>
+    plant.id === id ? {...plant, soldOut: updatedPlant.soldOut } : plant
+    )
+    setPlants(updatedPlants)
+  })
+}
+
+  const filteredPlants = plants.filter((plant) => 
+    plant.name.toLowerCase().includes(SearchTerm.toLowerCase())
+  )
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search SearchTerm={SearchTerm} onSearchChange={setSearchTerm}/>
+      <PlantList plants={filteredPlants} onToggleSoldOut={handleToggleSoldOut} />
     </main>
   );
 }
